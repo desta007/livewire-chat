f78eac624910736a75e48731ab90e103a08e0359
Desta <detajuventini@gmail.com>
2024-10-05 14:15:41 +0700

    chat realtime

---
+++ app/Livewire/Chat.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Message;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Builder;
+use Livewire\Component;
+
+class Chat extends Component
+{
+    public User $user;
+    public $message = '';
+
+    public function render()
+    {
+        return view('livewire.chat', [
+            'messages' => Message::where(function (Builder $query) {
+                $query->where('from_user_id', auth()->id())
+                    ->where('to_user_id', $this->user->id);
+            })->orWhere(function (Builder $query) {
+                $query->where('from_user_id', $this->user->id)
+                    ->where('to_user_id', auth()->id());
+            })->get(),
+            // 'messages' => Message::where('from_user_id', auth()->id())
+            //     ->orWhere('from_user_id', $this->user->id)
+            //     ->orWhere('to_user_id', auth()->id())
+            //     ->orWhere('to_user_id', $this->user->id)
+            //     ->get(),
+        ]);
+    }
+
+    public function sendMessage()
+    {
+        Message::create([
+            'from_user_id' => auth()->id(),
+            'to_user_id' => $this->user->id,
+            'message' => $this->message
+        ]);
+
+        $this->reset('message');
+    }
+}
